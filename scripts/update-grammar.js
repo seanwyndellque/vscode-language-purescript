26175ec155b409ebea140e7fca723d1acc4ae327
Sean Wyndell Que <seanwyndellque@gmail.com>
2019-08-16 15:41:59 +0800

    add scopes for [], {}

--- scripts/update-grammar.js
+++ scripts/update-grammar.js
@@ -3,14 +3,15 @@ var cson = require('cson');
 var process = require('process');
 var fs = require('fs');
 
-var url = 'https://raw.githubusercontent.com/purescript-contrib/atom-language-purescript/master/grammars/purescript.cson';
-request(url, function (error, response, body) {
-	if (error) {
-		console.error(error);
-		process.exit(1);
-	} else {
-		var obj = cson.parse(body);
-		var json = cson.createJSONString(obj);
-		fs.writeFileSync('syntaxes/purescript.json', json)
-	}
+var url =
+  'https://raw.githubusercontent.com/seanwyndellque/atom-language-purescript/add-more-highlights/grammars/purescript.cson';
+request(url, function(error, response, body) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  } else {
+    var obj = cson.parse(body);
+    var json = cson.createJSONString(obj);
+    fs.writeFileSync('syntaxes/purescript.json', json);
+  }
 });
